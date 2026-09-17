4274c12428914c9f1f7d904aa2e7dc889c14fc7a
akshayzagad <akshayzagade62@gmail.com.com>
2026-09-17 18:51:14 +0530

    feat: add CORS and security middleware (helmet, express-rate-limit) to enhance API security

--- src/app.js
+++ src/app.js
@@ -7,5 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'))
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use("/api/auth",authRouter)
 export default app;
