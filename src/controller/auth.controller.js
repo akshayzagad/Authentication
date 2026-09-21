cb782577a6a429c56ef4335770c45bb05473d78e
akshayzagad <akshayzagade62@gmail.com.com>
2026-09-21 22:51:29 +0530

    refactor: return user object instead of individual properties in loggedIn response

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -162,8 +162,9 @@ export async function loggedIn(req, res) {
 
   res.status(200).json({
     message: "User fetched successfully",
-    username: user.name,
-    email: user.email,
+    // username: user.name,
+    // email: user.email,
+    user
   });
 }
 
