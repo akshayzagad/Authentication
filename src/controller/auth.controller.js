c3fc4078361b2d433c30a23264042f21310f0956
akshayzagad <akshayzagade62@gmail.com.com>
2026-09-24 21:08:54 +0530

    fix: extend access token expiration time from 1 minute to 50 minutes in login function

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -131,7 +131,7 @@ export async function login(req, res) {
     { id: user._id, sessionId: session._id },
     config.JWT_SECRET,
     {
-      expiresIn: "1m",
+      expiresIn: "50m",
     },
   );
 
